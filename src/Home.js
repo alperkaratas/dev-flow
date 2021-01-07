a05d0ba1e4f25f75ddfce2badae3058146dbad63
Alper Karataş <alpkrts3@gmail.com>
2021-01-08 01:19:56 +0300

    Update ui

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,37 @@
 import "./App.css";
 import "semantic-ui-css/semantic.min.css";
-import { Button, Icon, Input, Card, Feed } from "semantic-ui-react";
+import React, { useEffect, useState } from "react";
+import { Button, Icon, Input, Card, Message, Image } from "semantic-ui-react";
 import logo from "./logo.png";
+import axios from "axios";
 
 const Home = () => {
+  const [githubSearch, setGithubSearch] = useState("");
+  const [searchBarText, setSearchBarText] = useState("");
+  const [getUserData, setGetUserData] = useState([{}]);
+  const githubAvatar = `https://avatars.githubusercontent.com/${githubSearch}`;
+
+  const getUsersData = async () => {
+    const response = await axios.get(
+      `https://api.github.com/users/${githubSearch}`,
+      {
+        headers: {
+          Authorization: "9b44cf6406e0ef91c3f2d006162719a8722a59e2",
+        },
+      }
+    );
+    setGetUserData(response.data);
+  };
+
+  const getAllData = () => {
+    getUsersData();
+    setGithubSearch("alperkaratas");
+  };
+
+  useEffect(() => {
+    getAllData();
+  });
+
   return (
     <div className="App">
       <div className="App-logo">
@@ -38,6 +66,7 @@ const Home = () => {
           <Icon name="twitter" /> Twitter
         </Button>
         <Button
+          className="search-logo"
           onClick={() => window.open("mailto:alpkrts3@gmail.com")}
           color="red"
         >
@@ -45,147 +74,118 @@ const Home = () => {
         </Button>
       </div>
       <div style={{ display: "flex", flexDirection: "row" }}>
-        <div style={{ marginRight: 100, alignItems: "center" }}>
-          <Input
-            size="big"
-            icon="github"
-            iconPosition="left"
-            placeholder="Search users..."
-          />
-
-          <Card className="card-container">
-            <Card.Content>
-              <Card.Header>Recent Activity</Card.Header>
-            </Card.Content>
-            <Card.Content>
-              <Feed>
-                <Feed.Event>
-                  <Feed.Label image="/images/avatar/small/jenny.jpg" />
-                  <Feed.Content>
-                    <Feed.Date content="1 day ago" />
-                    <Feed.Summary>
-                      You added <a>Jenny Hess</a> to your <a>coworker</a> group.
-                    </Feed.Summary>
-                  </Feed.Content>
-                </Feed.Event>
-
-                <Feed.Event>
-                  <Feed.Label image="/images/avatar/small/molly.png" />
-                  <Feed.Content>
-                    <Feed.Date content="3 days ago" />
-                    <Feed.Summary>
-                      You added <a>Molly Malone</a> as a friend.
-                    </Feed.Summary>
-                  </Feed.Content>
-                </Feed.Event>
-
-                <Feed.Event>
-                  <Feed.Label image="/images/avatar/small/elliot.jpg" />
-                  <Feed.Content>
-                    <Feed.Date content="4 days ago" />
-                    <Feed.Summary>
-                      You added <a>Elliot Baker</a> to your <a>musicians</a>{" "}
-                      group.
-                    </Feed.Summary>
-                  </Feed.Content>
-                </Feed.Event>
-              </Feed>
-            </Card.Content>
-          </Card>
-        </div>
-        <div style={{ marginRight: 100 }}>
-          <Input
-            size="big"
-            icon="medium"
-            iconPosition="left"
-            placeholder="Search users..."
-          />
-          <Card className="card-container">
-            <Card.Content>
-              <Card.Header>Recent Activity</Card.Header>
-            </Card.Content>
-            <Card.Content>
-              <Feed>
-                <Feed.Event>
-                  <Feed.Label image="/images/avatar/small/jenny.jpg" />
-                  <Feed.Content>
-                    <Feed.Date content="1 day ago" />
-                    <Feed.Summary>
-                      You added <a>Jenny Hess</a> to your <a>coworker</a> group.
-                    </Feed.Summary>
-                  </Feed.Content>
-                </Feed.Event>
-
-                <Feed.Event>
-                  <Feed.Label image="/images/avatar/small/molly.png" />
-                  <Feed.Content>
-                    <Feed.Date content="3 days ago" />
-                    <Feed.Summary>
-                      You added <a>Molly Malone</a> as a friend.
-                    </Feed.Summary>
-                  </Feed.Content>
-                </Feed.Event>
-
-                <Feed.Event>
-                  <Feed.Label image="/images/avatar/small/elliot.jpg" />
-                  <Feed.Content>
-                    <Feed.Date content="4 days ago" />
-                    <Feed.Summary>
-                      You added <a>Elliot Baker</a> to your <a>musicians</a>{" "}
-                      group.
-                    </Feed.Summary>
-                  </Feed.Content>
-                </Feed.Event>
-              </Feed>
-            </Card.Content>
-          </Card>
-        </div>
         <div>
-          <Input
-            size="big"
-            icon="twitter"
-            iconPosition="left"
-            placeholder="Search users..."
-          />
-          <Card className="card-container">
-            <Card.Content>
-              <Card.Header>Recent Activity</Card.Header>
-            </Card.Content>
-            <Card.Content>
-              <Feed>
-                <Feed.Event>
-                  <Feed.Label image="/images/avatar/small/jenny.jpg" />
-                  <Feed.Content>
-                    <Feed.Date content="1 day ago" />
-                    <Feed.Summary>
-                      You added <a>Jenny Hess</a> to your <a>coworker</a> group.
-                    </Feed.Summary>
-                  </Feed.Content>
-                </Feed.Event>
+          <div className="search-button">
+            <Input
+              size="big"
+              icon="github"
+              iconPosition="left"
+              placeholder="Search users..."
+              onChange={(text) => setSearchBarText(text)}
+            />
+            <div style={{ marginLeft: 5 }}>
+              <Button onClick={() => getAllData()} circular icon="search" />
+            </div>
+          </div>
+          {githubSearch === "" ? (
+            <div
+              style={{
+                margin: 20,
+                marginTop: 80,
+                display: "flex",
+                justifyContent: "center",
+              }}
+            >
+              <Message info>
+                <Message.Header className="text-25">
+                  How it works?
+                </Message.Header>
+                <p style={{ marginTop: 10 }}>
+                  Enter the username of the person whose GitHub status you want
+                  to see and review it :)
+                </p>
+              </Message>
+            </div>
+          ) : (
+            <div>
+              <div style={{ margin: 20, marginTop: 50 }}>
+                <div
+                  style={{
+                    margin: 20,
+                    marginTop: 50,
+                    display: "flex",
+                    justifyContent: "center",
+                  }}
+                >
+                  <Card>
+                    <Image src={githubAvatar} wrapped ui={false} />
+                    <Card.Content>
+                      <Card.Header>{githubSearch}</Card.Header>
+                      <Card.Meta>
+                        <span className="date">{getUserData.name}</span>
+                      </Card.Meta>
+                      <Card.Description>{getUserData.bio}</Card.Description>
+                      <Card.Description className="user-location">
+                        {getUserData.location}
+                      </Card.Description>
+                    </Card.Content>
+                  </Card>
+                </div>
 
-                <Feed.Event>
-                  <Feed.Label image="/images/avatar/small/molly.png" />
-                  <Feed.Content>
-                    <Feed.Date content="3 days ago" />
-                    <Feed.Summary>
-                      You added <a>Molly Malone</a> as a friend.
-                    </Feed.Summary>
-                  </Feed.Content>
-                </Feed.Event>
-
-                <Feed.Event>
-                  <Feed.Label image="/images/avatar/small/elliot.jpg" />
-                  <Feed.Content>
-                    <Feed.Date content="4 days ago" />
-                    <Feed.Summary>
-                      You added <a>Elliot Baker</a> to your <a>musicians</a>{" "}
-                      group.
-                    </Feed.Summary>
-                  </Feed.Content>
-                </Feed.Event>
-              </Feed>
-            </Card.Content>
-          </Card>
+                <Card.Group itemsPerRow={3}>
+                  <Card
+                    color="red"
+                    image={
+                      "https://github-readme-stats.vercel.app/api/pin/?username=alperkaratas&repo=find-mentor-mobil"
+                    }
+                  />
+                  <Card
+                    color="orange"
+                    image={
+                      "https://github-readme-stats.vercel.app/api/pin/?username=alperkaratas&repo=find-mentor-mobil"
+                    }
+                  />
+                  <Card
+                    color="yellow"
+                    image={
+                      "https://github-readme-stats.vercel.app/api/pin/?username=alperkaratas&repo=find-mentor-mobil"
+                    }
+                  />
+                  <Card
+                    color="olive"
+                    image={
+                      "https://github-readme-stats.vercel.app/api/pin/?username=alperkaratas&repo=find-mentor-mobil"
+                    }
+                  />
+                  <Card
+                    color="green"
+                    image={
+                      "https://github-readme-stats.vercel.app/api/pin/?username=alperkaratas&repo=find-mentor-mobil"
+                    }
+                  />
+                  <Card
+                    color="teal"
+                    image={
+                      "https://github-readme-stats.vercel.app/api/pin/?username=alperkaratas&repo=find-mentor-mobil"
+                    }
+                  />
+                </Card.Group>
+              </div>
+              <div
+                style={{
+                  margin: 20,
+                  marginTop: 50,
+                  display: "flex",
+                  justifyContent: "center",
+                }}
+              >
+                <Card
+                  color="teal"
+                  image={`https://github-readme-stats.vercel.app/api/top-langs/?username=${githubSearch}`}
+                />
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </div>
